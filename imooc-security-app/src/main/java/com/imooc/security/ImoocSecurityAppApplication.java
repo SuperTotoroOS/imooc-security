09337cc4e91ee549196f0d33db46bc41b8afc0af
杨蕴琦 <yunqi.yang@shijue.me>
2017-09-12 15:55:42 +0800

    开始

---
+++ imooc-security-app/src/main/java/com/imooc/security/ImoocSecurityAppApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSecurityAppApplication.class, args);
+	}
+}
